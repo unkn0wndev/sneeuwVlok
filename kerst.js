c44f4e3f8cc2dd5b0804070ca9dbab30b97bf3ec
Tim van Osch <timvanosch2001@gmail.com>
2019-12-16 10:02:03 +0100

    commit 2

--- kerst.js
+++ kerst.js
@@ -1,5 +1,28 @@
-let wens = document.createElement('div');
 
-wens.innerHTML = codeSVGvlok;
 
-document.body.appendChild(wens);
+class Wens {
+   constructor( horizontaal, verticaal, snelheid, kleur ) {
+      this._hori  = horizontaal;
+      this._vert  = verticaal;
+      this._snelh = snelheid;
+
+   }
+   // method maken: creëer een element en plaats deze in de body
+   maken() {
+      let wens = document.createElement('div');
+      wens.innerHTML = codeSVGvlok;
+      wens.className  = 'wens';
+      wens.style.top  = this._vert + 'px';
+      wens.style.left = this._hori + 'px';
+
+      document.body.appendChild(wens);
+   }
+}
+
+// twee instanties van de class Auto maken
+let vlokje1 = new Wens(Math.random()*200, Math.random()*500, 4);
+vlokje1.maken();
+let vlokje2 = new Wens(Math.random()*200, Math.random()*500, 4);
+vlokje2.maken();
+let vlokje3 = new Wens(Math.random()*200, Math.random()*500, 4);
+vlokje3.maken();
